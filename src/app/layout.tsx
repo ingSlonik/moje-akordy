66fcd67835dbe14fdcce907fda0c915d5d6fe33e
Filip Paulů <ing.fenix@seznam.cz>
2024-07-19 16:26:04 +0200

    Add new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
+import Head from "next/head";
+import Image from "next/image";
+
+import logo from "./icon.png";
+import Link from "next/link";
+
 const openSans = Open_Sans({ subsets: ["latin", "latin-ext"] });
 
 export const metadata: Metadata = {
@@ -16,7 +22,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="cs">
-      <body className={openSans.className}>{children}</body>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body className={openSans.className}>
+        <nav>
+          <div className="content">
+            <Link href="/">
+              <Image className="logo" alt="Moje akordy Logo" src={logo} width={64} height={64} />
+              <span>Moje akordy</span>
+            </Link>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
