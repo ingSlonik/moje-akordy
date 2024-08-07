be2baf349c93419c4e81535779fd4a7b72ce1106
Filip Paulů <ing.fenix@seznam.cz>
2024-08-07 21:52:03 +0200

    Update ranking display

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,3 +1,17 @@
 export default function Ranking({ ranking, size = 16 }: { ranking: number, size?: number }) {
-    return <div className="ranking" style={{ height: size, width: `${Math.round(size * ranking / 100 * 5)}px` }} />;
+
+    return <div>
+        <div
+            className="ranking"
+            style={{
+                display: "inline-block",
+                height: size,
+                width: `${Math.round(size * ranking / 100 * 5)}px`,
+            }}
+        />
+        {ranking <= 20 && <span
+            style={{ fontSize: "12px", fontStyle: "italic", color: "gray" }}>
+            Učím se
+        </span>}
+    </div>;
 }
